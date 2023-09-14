daf647204fe6799e7fbf2cb313eb1022691b9a05
MediaLab <MediaLab@DESKTOP-DC2C976>
2023-09-14 14:55:32 +0900

    2023.9.14

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -1,4 +1,4 @@
-
+/*
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, World! ");
@@ -7,4 +7,22 @@ public class Hello {
 		System.out.print("User name is JJH");
 	}
 
+}
+*/
+
+public class Hello {
+	public static int sum(int n, int m) {
+		return n+m;
+	}
+	public static void main(String[] args) {
+		int i = 20;
+		int s;
+		char a;
+		
+		s = sum(i, 10);
+		a = '?';
+		System.out.println(a);
+		System.out.println("Hello");
+		System.out.println(s);
+	}
 }
