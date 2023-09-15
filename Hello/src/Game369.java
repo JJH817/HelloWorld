60ae8e2511f1a7adef1f2b935008c0c8d5fd9038
MediaLab <MediaLab@DESKTOP-DC2C976>
2023-09-15 11:44:25 +0900

    Homework2 준비중

---
+++ Hello/src/Game369.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Game369 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+/*
+
+switch((num/10) > 0);					//99 66 33의 경우도 짜야됨 다시 코드 작성할것
+	if((num%10) % 3 == 0)
+	System.out.println("박수짝짝
+
+*/
